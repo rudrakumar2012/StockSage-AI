c869ff4454fdf761361b6c99104e3e51753e0b61
rudrakumar2012 <manaw10071994@gmail.com>
2024-07-29 17:18:26 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000' // For development
+      '/api': 'https://stock-sage-ai.vercel.app/' // For development
     }
   },
   build: {
