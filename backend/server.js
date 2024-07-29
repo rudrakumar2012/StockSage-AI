35597a3a78e584304d0e79ec2ad6a29a38da31f9
rudrakumar2012 <manaw10071994@gmail.com>
2024-07-29 17:32:34 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -112,15 +112,6 @@ app.get("/api/nifty50", async (req, res) => {
   }
 });
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/dist")));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist/index.html"));
-});
-
 // Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
