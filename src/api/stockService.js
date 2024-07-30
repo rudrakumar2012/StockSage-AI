ad370a9c0ee61dcd447d31153ef094bd191b0bae
rudrakumar2012 <manaw10071994@gmail.com>
2024-07-30 17:05:47 +0530

    fix

--- src/api/stockService.js
+++ src/api/stockService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const api_url = process.env.API_URL;
+const API_URL = "https://stocksage-ai.onrender.com/api";
 
 export const fetchStockData = async () => {
   try {
-    const response = await axios.get(`${api_url}/stocks`);
+    const response = await axios.get(`${API_URL}/stocks`);
     return response.data;
   } catch (error) {
     console.error("Error fetching stock data:", error);
